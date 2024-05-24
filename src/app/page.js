3a8f456173e9b5596dfac0d1d8386898718682b4
Mark Grimes <mgrimes@cpan.org>
2024-05-24 14:07:11 -0400

    feat: minimal reproduction of error
    Removes tailwind and daisyui. Removes postcss config. Simplyfies the
    HTML/CSS.

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-
-        <div role="tablist" className="tabs tabs-lifted">
-          <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" defaultChecked/>
-          <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
-
-          <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 2" />
-          <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
-
-          <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 3" />
-          <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 3</div>
-        </div>
-
-      </div>
+    <main>
+      <input type="checkbox" className="cb" name="my_cb" defaultChecked />
+      Label
     </main>
   );
 }
-
